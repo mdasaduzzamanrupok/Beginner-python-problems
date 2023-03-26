daa08f55172fdbb56e9db22c07d064feaac9186f
MD ASADUZZAMAN <121396092+mdasaduzzamanrupok@users.noreply.github.com>
2023-03-26 11:42:39 +0600

    Celsius and Fahrenheit

---
+++ Celsius Fahrenheit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#Celsius to Fahrenheit
+#(c*1.8)+32
+
+celsius = float(input("Enter your celsius value: "))
+
+fahrenheit = (celsius*1.8)+32
+
+print("Fahrenheit is ",fahrenheit)
+
+
+# In[5]:
+
+
+# Fahrenheit to Centigrade 
+#(f-32)/1.8
+
+fahrenheit = float(input("Enter your fahrenheit value: "))
+
+centigrade = (fahrenheit-32)/1.8
+
+print("Centigrade is : ", centigrade)
+
+
+# In[ ]:
+
+
+
+
