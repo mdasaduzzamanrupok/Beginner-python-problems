46589c4fdde18458ba7930b5a38fc897994e1c60
MD ASADUZZAMAN <121396092+mdasaduzzamanrupok@users.noreply.github.com>
2023-03-26 11:43:59 +0600

    Update Celsius Fahrenheit.py

--- Celsius Fahrenheit.py
+++ Celsius Fahrenheit.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
 
 #Celsius to Fahrenheit
 #(c*1.8)+32
@@ -14,9 +9,6 @@ fahrenheit = (celsius*1.8)+32
 print("Fahrenheit is ",fahrenheit)
 
 
-# In[5]:
-
-
 # Fahrenheit to Centigrade 
 #(f-32)/1.8
 
@@ -26,8 +18,6 @@ centigrade = (fahrenheit-32)/1.8
 
 print("Centigrade is : ", centigrade)
 
-
-# In[ ]:
 
 
 
