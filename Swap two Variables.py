f873a751def9cdc38e3b455a08763ac196fe9ce2
MD ASADUZZAMAN <121396092+mdasaduzzamanrupok@users.noreply.github.com>
2023-03-26 10:51:26 +0600

    Swap two variables

---
+++ Swap two Variables.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#swap two variables
+
+a = 5 
+b = 6 
+
+print(a,b)
+
+t = a 
+a = b 
+b = t
+
+print(a,b)
+
+
+# In[4]:
+
+
+a = 5 
+b = 6 
+
+print(a,b)
+
+a,b = b,a
+
+print(a,b)
+
+
+# In[5]:
+
+
+a = 5
+b = 6 
+
+print(a,b)
+a = a+b
+b = a-b
+a = a-b
+print(a,b)
+
+
+# In[ ]:
+
+
+
+
